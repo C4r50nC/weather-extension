ca0d74e46e5063068aad1dd056f5b74787603a5a
C4r50nC <xichen059@gmail.com>
2024-04-05 21:28:57 +1100

    feat: add interface for weather data api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,14 +1,37 @@
 const OPEN_WEATHER_API_KEY = "207850f3db5825e2fa7b27413323b07b"; // Visible to the public, only free API keys are allowed
 
-export async function fetchOpenWeatherData(city: string): Promise<any> {
+export interface OpenWeatherData {
+  name: string;
+  main: {
+    feels_like: number;
+    humidity: number;
+    temp: number;
+    temp_max: number;
+    temp_min: number;
+  };
+  weather: {
+    description: string;
+    icon: string;
+    id: number;
+    main: string;
+  }[];
+  wind: {
+    deg: number;
+    speed: number;
+  };
+}
+
+export async function fetchOpenWeatherData(
+  city: string
+): Promise<OpenWeatherData> {
   const res = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPEN_WEATHER_API_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${OPEN_WEATHER_API_KEY}`
   );
 
   if (!res.ok) {
     throw new Error("City not found");
   }
 
-  const data = await res.json();
+  const data: OpenWeatherData = await res.json();
   return data;
 }
