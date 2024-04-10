a96729c5135327da6da3e7bf4e26d01df5b7cbdc
C4r50nC <xichen059@gmail.com>
2024-04-10 22:10:30 +1000

    feat: implement add city function and delete city function

--- src/popup/weather-card/weather-card.tsx
+++ src/popup/weather-card/weather-card.tsx
@@ -1,14 +1,29 @@
 import React, { useEffect, useState } from "react";
-import { Box, Card, CardContent, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@mui/material";
 import { fetchOpenWeatherData, OpenWeatherData } from "../../utils/api";
 
 const WeatherCardContainer: React.FC<{
   children: React.ReactNode;
-}> = ({ children }) => {
+  onDelete?: () => void;
+}> = ({ children, onDelete }) => {
   return (
     <Box mx={"4px"} my={"16px"}>
       <Card>
         <CardContent>{children}</CardContent>
+        <CardActions>
+          {onDelete && (
+            <Button color="secondary" onClick={onDelete}>
+              Delete
+            </Button>
+          )}
+        </CardActions>
       </Card>
     </Box>
   );
@@ -18,7 +33,8 @@ type WeatherCardState = "loading" | "error" | "ready";
 
 const WeatherCard: React.FC<{
   city: string;
-}> = ({ city }) => {
+  onDelete?: () => void;
+}> = ({ city, onDelete }) => {
   const [weatherData, setWeatherData] = useState<OpenWeatherData | null>(null);
   const [cardState, setCardState] = useState<WeatherCardState>("loading");
 
@@ -33,7 +49,7 @@ const WeatherCard: React.FC<{
 
   if (cardState === "loading" || cardState === "error") {
     return (
-      <WeatherCardContainer>
+      <WeatherCardContainer onDelete={onDelete}>
         <Typography variant="body1">
           {cardState === "loading"
             ? "Loading..."
@@ -44,7 +60,7 @@ const WeatherCard: React.FC<{
   }
 
   return (
-    <WeatherCardContainer>
+    <WeatherCardContainer onDelete={onDelete}>
       <Typography variant="h5">{weatherData.name}</Typography>
       <Typography variant="body1">
         {Math.round(weatherData.main.temp)}
