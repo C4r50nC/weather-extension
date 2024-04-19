7147134cd5df1fc257380fccc464dac247b09828
C4r50nC <xichen059@gmail.com>
2024-04-19 21:36:45 +1000

    feat: add badge text to display temperature of home city

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,8 +1,10 @@
 import {
   getStoredCities,
+  getStoredOptions,
   setStoredCities,
   setStoredOptions,
 } from "../utils/storage";
+import { fetchOpenWeatherData } from "../utils/api";
 
 chrome.runtime.onInstalled.addListener(() => {
   setStoredCities([]);
@@ -19,8 +21,28 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
+const refreshFrequencyInMinutes = 60;
+chrome.alarms.create({
+  periodInMinutes: refreshFrequencyInMinutes,
+});
+
 chrome.contextMenus.onClicked.addListener((event) => {
   getStoredCities().then((cities) => {
     setStoredCities([...cities, event.selectionText]);
+  });
+});
+
+chrome.alarms.onAlarm.addListener(() => {
+  getStoredOptions().then((options) => {
+    if (options.homeCity === "") {
+      return;
+    }
+    fetchOpenWeatherData(options.homeCity, options.tempScale).then((data) => {
+      const temp = Math.round(data.main.temp);
+      const symbol = options.tempScale === "metric" ? "\u2103" : "\u2109";
+      chrome.action.setBadgeText({
+        text: `${temp}${symbol}`,
+      });
+    });
   });
 });
